a8c4aa4f482f7c6bfdb4fab30a0220feeacae7bc
madhyam bharati <96839514+Madhyam123@users.noreply.github.com>
2023-07-29 19:42:51 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -13,20 +13,20 @@ export default function Home() {
 
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect( () => {
-    (
-      async () => {
-          const LocomotiveScroll = (await import('locomotive-scroll')).default
-          const locomotiveScroll = new LocomotiveScroll();
+  // useEffect( () => {
+  //   (
+  //     async () => {
+  //         const LocomotiveScroll = (await import('locomotive-scroll')).default
+  //         const locomotiveScroll = new LocomotiveScroll();
 
-          setTimeout( () => {
-            setIsLoading(false);
-            document.body.style.cursor = 'default'
-            window.scrollTo(0,0);
-          }, 2000)
-      }
-    )()
-  }, [])
+  //         setTimeout( () => {
+  //           setIsLoading(false);
+  //           document.body.style.cursor = 'default'
+  //           window.scrollTo(0,0);
+  //         }, 2000)
+  //     }
+  //   )()
+  // }, [])
 
   return (
     <main className={styles.main}>
